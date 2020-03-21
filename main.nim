4d423b341be3eba3fbeefb011eaa7b1a4a509fc4
leventev <levente.vamos@protonmail.com>
2020-03-21 16:55:08 +0100

    Add basic options

---
+++ main.nim
@@ -0,0 +1,39 @@
+import json, httpclient, times, strformat
+import options
+
+Parse()
+
+var 
+  requestURL = "https://corona.lmao.ninja/"
+  client = newHttpClient()
+  node: JsonNode
+  countryMode = false
+
+if country == "":
+  requestURL &= "all"
+else:
+  requestURL &= "countries/" & country
+  countryMode = true
+
+let data = client.getContent(requestURL)
+
+node = parseJson(data)
+
+var
+  cases      = node["cases"].getInt()
+  deaths     = node["deaths"].getInt()
+  recovered  = node["recovered"].getInt()
+  lastUpdated: string
+  output: string
+
+if not countryMode:
+  let unixStart = parse("1970-01-01", "yyyy-MM-dd")
+  let now = initDuration(milliseconds = node["updated"].getInt())
+
+  lastUpdated = $(unixStart + now)
+
+  output = fmt"cases: {cases} deaths: {deaths} recovered: {recovered} last updated: {lastUpdated}"
+else:
+  output = fmt"cases: {cases} deaths: {deaths} recovered: {recovered}"
+
+echo output
---
+++ options.nim
@@ -0,0 +1,69 @@
+import parseopt, os, strutils, strformat
+import globals
+
+type
+  OptionCallback* = proc(val: string)
+
+  Option* = object
+    long*: string
+    short*: string
+    description*: string
+    callback*: OptionCallback
+
+var
+  options*: seq[Option]
+  country* = ""
+
+proc GetOptionLength(option: Option): int =
+  return (option.short & ", " & option.long).len()
+
+proc GetLongestOption(): int =
+  result = 0
+  for option in options:
+    let len = GetOptionLength(option)
+    if len > result:
+      result = len
+
+proc PrintVersion() =
+  echo &"Coronastat Version {version}"
+  echo &"Compiled at {CompileDate} {CompileTime}"
+  echo &"Author: {author}"
+
+proc VersionCallback(val: string) =
+  PrintVersion()
+
+proc HelpCallback(val: string) =
+  PrintVersion()
+  echo "\nOptions:"
+
+  for option in options:
+    stdout.write(&"\t-{option.short}, --{option.long}")
+    
+    let descPos = GetLongestOption() + 4
+    let diff = descPos - GetOptionLength(option)
+
+    stdout.write(' '.repeat(diff))
+    echo option.description
+
+proc Parse*() =
+  options.add(Option(long: "version", short: "v", description: "show version info", callback: VersionCallback))
+  options.add(Option(long: "help", short: "h", description: "show help", callback: HelpCallback))
+  options.add(Option(long: "country", short: "c", description: "set country", callback: (proc (val: string) = country = val)))
+
+  let args = commandLineParams().join(" ")
+
+  var opt = initOptParser(args)
+
+  while true:
+    opt.next()
+    case opt.kind
+      of cmdEnd: break
+      of cmdShortOption:
+        for option in options:
+          if option.short == opt.key:
+            option.callback(opt.val)
+      of cmdLongOption: 
+        for option in options:
+          if option.long == opt.key:
+            option.callback(opt.val)
+      of cmdArgument: break
